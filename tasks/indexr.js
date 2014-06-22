ad45ccd133c0645755a30f9cd6671b424f027dd4
Timo Mayer <timo.mayer@MANDARINFISCH.virtual-identity.net>
2014-06-22 11:55:59 +0200

    stripnumbers

--- tasks/indexr.js
+++ tasks/indexr.js
@@ -21,52 +21,59 @@ module.exports = function (grunt) {
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      template: false,
-	    meta_data_separator: /\r?\n\r?\n/,
-      replaceInFilePath: ""
+		template: false,
+		meta_data_separator: /\r?\n\r?\n/,
+		replaceInFilePath: ""
     });
 	
 	var parse = parseSetup(options.meta_data_separator);
 
+	var stripNumbers = function (sString) {
+		if (sString.length > 3) {
+			return sString.substring(0,3).replace(/[0-9]/g, '') + sString.substring(3);
+		}
+		return sString;
+	};
+
     var templateList = [];
-	  var keyMap = {};
+	var keyMap = {};
     // Iterate over all specified file groups.
     this.files.forEach(function (file) {
       // Concat specified files.
         file.src.filter(function (filepath) {
         // Warn on and remove invalid source files (if nonull was set).
 
-          if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-		  var readHeader = _.compose(parse.header, grunt.file.read);
-		  var header  = readHeader(filepath);
-		  
-		  var filepathSplit = filepath.split('/');
-		  filepathSplit = _.last(filepathSplit).split('.');
-		  if(filepathSplit.length !== 3) {
-			  filepathSplit.unshift("x");
-		  }
-		  if(typeof keyMap[filepathSplit[0]] === 'undefined') {
-			keyMap[filepathSplit[0]] = templateList.length;
-			templateList[keyMap[filepathSplit[0]]] = { name: filepathSplit[0], namespaceDescription: (typeof header.namespaceDescription === 'undefined') ? "" : header.namespaceDescription,  items: []};
-		  }
-		  templateList[keyMap[filepathSplit[0]]].items.push({fileName:filepath.replace(options.replaceInFilePath, ''), readName: filepathSplit[1], header: header});
-          return true;
+		if (!grunt.file.exists(filepath)) {
+			grunt.log.warn('Source file "' + filepath + '" not found.');
+			return false;
+		} else {
+			var readHeader = _.compose(parse.header, grunt.file.read);
+			var header  = readHeader(filepath);
+		
+			var filepathSplit = filepath.split('/');
+			filepathSplit = _.last(filepathSplit).split('.');
+			if(filepathSplit.length !== 3) {
+				filepathSplit.unshift("_");
+			}
+			if(typeof keyMap[filepathSplit[0]] === 'undefined') {
+				keyMap[filepathSplit[0]] = templateList.length;
+				templateList[keyMap[filepathSplit[0]]] = { name: stripNumbers(filepathSplit[0]), namespaceDescription: (typeof header.namespaceDescription === 'undefined') ? "" : stripNumbers(header.namespaceDescription),  items: []};
+			}
+			templateList[keyMap[filepathSplit[0]]].items.push({fileName: filepath.replace(options.replaceInFilePath, ''), readName: stripNumbers(filepathSplit[1]), header: header});
+			return true;
         }
       });
 
     });
-      var templateSettings = {
+	var templateSettings = {
 		strip: false
-	  };
-	  var settings = _.extend({}, dot.templateSettings, templateSettings);
-      var src = grunt.file.read(options.template);
-      var template = dot.template(src, settings);
-      var output = template(_.extend({}, {indexr: templateList}));
+	};
+	var settings = _.extend({}, dot.templateSettings, templateSettings);
+	var src = grunt.file.read(options.template);
+	var template = dot.template(src, settings);
+	var output = template(_.extend({}, {indexr: templateList}));
 
-      grunt.file.write('.tmp/index.html', output);
+	grunt.file.write('.tmp/index.html', output);
   });
 
 };
